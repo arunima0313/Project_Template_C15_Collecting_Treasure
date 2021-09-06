a75446deeab567ec121608e3d2016591dd12a871
arunima0313 <88248700+arunima0313@users.noreply.github.com>
2021-09-06 16:07:41 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -65,15 +65,20 @@ function draw() {
     }
     else if (diamondsG.isTouching(boy)) {
       diamondsG.destroyEach();
-
+      treasureCollection = treasureCollection + 100;
       
     }else if(jwelleryG.isTouching(boy)) {
       jwelleryG.destroyEach();
-
+      treasureCollection = treasureCollection + 150;
       
     }else{
       if(swordGroup.isTouching(boy)) {
-
+        gameState = END;
+        boy.addAnimation("SahilRunning", endImg);
+        boy.x = 200;
+        boy.y = 300
+        cashG.destroyEach();
+        cashG.setVelocityYEach(0);
     }
   }
   
